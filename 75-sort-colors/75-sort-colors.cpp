eb6f62554efd38275b1e18cbabdb1236268e5f50
Rajath V <f20190122@goa.bits-pilani.ac.in>
2022-02-01 16:15:07 +0530

    Time: 4 ms (45.87%), Space: 8.1 MB (91.43%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int red = 0, blue = 0, green = 0;
+        for(auto x : nums){
+            if(x == 0) red++;
+            if(x == 1) blue++;
+            if(x == 2) green++;
+        }
+        int idx = 0;
+        while(red--) nums[idx++] = 0;
+        while(blue--) nums[idx++] = 1;
+        while(green--) nums[idx++] = 2;
         
     }
 };
