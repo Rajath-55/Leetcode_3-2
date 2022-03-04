929bfb1b3b3c1aedd1644b8b2fcc0f166732d359
Rajath V <f20190122@goa.bits-pilani.ac.in>
2022-03-04 21:38:57 +0530

    Time: 4 ms (54.21%), Space: 7.1 MB (65.36%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int>temp;
+    
+    //recursive solution O(2^n) time.
     void recurse(vector<vector<int>>&ans, vector<int>nums, int index){
         if(index == nums.size()){
             ans.push_back(temp);
@@ -14,9 +16,28 @@ public:
         
         
     }
+    
+    void iterative(vector<vector<int>>&ans, vector<int>nums){
+        int size = 1 << (int)nums.size();
+        
+        for(int i = 0; i < size; ++i){
+            int k = i;
+            vector<int>temp;
+            int idx = 0;
+            while(k){
+                int bit = k&1;
+                if(bit) temp.push_back(nums[idx]);
+                idx++;
+                k >>=1;
+            }
+            ans.push_back(temp);
+            
+        }
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>>ans;
-        recurse(ans, nums, 0);
+        // recurse(ans, nums, 0);
+        iterative(ans, nums);
         return ans;
     }
 };
