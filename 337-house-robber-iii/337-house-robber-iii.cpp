298cd1e4e984410753ff30f2c8f9732adf2859b8
Rajath V <f20190122@goa.bits-pilani.ac.in>
2022-03-22 17:56:03 +0530

    Time: 12 ms (91.47%), Space: 24.9 MB (45.34%) - LeetHub

--- 337-house-robber-iii/337-house-robber-iii.cpp
+++ 337-house-robber-iii/337-house-robber-iii.cpp
@@ -14,19 +14,13 @@ public:
     unordered_map<TreeNode*,int> dp;
     
     int rob(TreeNode* root) {
-		// if the root is null then robber cant rob anything
-        if(root==NULL) return 0;
-        // if we already have the solution, we will return it
+        if(!root) return 0;
         if(dp.count(root)) return dp[root];
-        // grand children houses robbery
-        int left_left_robbery = root->left ? rob(root->left->left) : 0;
-        int left_right_robbery = root->left ? rob(root->left->right) : 0;
-        int right_left_robbery = root->right ? rob(root->right->left) : 0;
-        int right_right_robbery = root->right ? rob(root->right->right) : 0;
-        // 2 possible choices (current house + grand children houses) or (children houses)
-        int choice1 = root->val + left_left_robbery + left_right_robbery + right_left_robbery + right_right_robbery;
-        int choice2 = rob(root->left) + rob(root->right);
-        // store the result and return it.
-        return dp[root] = max(choice1,choice2);
+        
+        int takeRoot = root->val + (root->left ? rob(root->left->left) + rob(root->left->right) : 0) + (root->right ? rob(root->right->left) + rob(root->right->right) : 0);
+        int dontTakeRoot = rob(root->left) + rob(root->right);
+        
+        return dp[root] = max(takeRoot, dontTakeRoot);
     }
+    
 };
