d1fcd430c051d91d144ebf2b1dd0f7180fd2ea45
Rajath V <f20190122@goa.bits-pilani.ac.in>
2022-03-28 22:44:50 +0530

    Time: 0 ms (100.00%), Space: 6 MB (44.81%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        // double ans = 0.0;
-        return pow(x, n);
+        double ans = 1.0;
+        int temp = n;
+        long k = n;
+        if(n < 0) k*=-1;
+        while(k > 0){
+            if(k&1){
+                ans *= x;
+            }
+            k>>= 1;
+            x = x*x;
+        }
+        
+        return temp < 0 ? 1.0/ans : ans;
+        
     }
 };
