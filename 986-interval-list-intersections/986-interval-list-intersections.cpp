ce682bb45cc9aba0f6c0b1a1f89245449d78d338
Rajath V <f20190122@goa.bits-pilani.ac.in>
2022-04-06 00:58:53 +0530

    Time: 35 ms (80.34%), Space: 18.7 MB (77.99%) - LeetHub

---
+++ 986-interval-list-intersections/986-interval-list-intersections.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+    bool isIntersection(int oneX, int oneY, int twoX, int twoY){
+        return ((oneY >= twoX and oneX <= twoY) or (twoX >= oneX and twoX <= oneY));
+    }
+    
+    
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+      vector<vector<int>> res;
+        int n=firstList.size();
+        int m=secondList.size();
+        
+        int i=0,j=0;
+        while(i<n && j<m)
+        {
+            if(firstList[i][1]>=secondList[j][0] && firstList[i][0]<=secondList[j][1] ||
+               secondList[j][0]<=firstList[i][1] && secondList[j][1]>=firstList[i][0]){//intersection is present
+                int s=max(firstList[i][0],secondList[j][0]);
+                int l=min(firstList[i][1],secondList[j][1]);
+                res.push_back({s,l});
+            }
+
+                if(firstList[i][1]<secondList[j][1])
+                    i++;
+                else
+                    j++;
+        }
+        
+        
+       
+       
+        
+        return res;
+        
+    }
+};
