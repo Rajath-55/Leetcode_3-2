7c482b679a84dacdd97dd1806b0517b00af81a42
Rajath V <f20190122@goa.bits-pilani.ac.in>
2022-05-29 11:33:40 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (53.29%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,43 +1,20 @@
 class Solution {
 public:
-    vector<int>temp;
+    vector<vector<int>>ans;
     
-    //recursive solution O(2^n) time.
-    void recurse(vector<vector<int>>&ans, vector<int>nums, int index){
-        if(index == nums.size()){
+    void generate(vector<int>&nums, int idx, vector<int>&temp){
+        if(idx >= nums.size()){
             ans.push_back(temp);
             return;
         }
-        
-        temp.push_back(nums[index]);
-        recurse(ans, nums, index + 1);
+        temp.push_back(nums[idx]);
+        generate(nums, idx + 1, temp);
         temp.pop_back();
-        recurse(ans, nums, index + 1);
-        
-        
-    }
-    
-    void iterative(vector<vector<int>>&ans, vector<int>nums){
-        int size = 1 << (int)nums.size();
-        
-        for(int i = 0; i < size; ++i){
-            int k = i;
-            vector<int>temp;
-            int idx = 0;
-            while(k){
-                int bit = k&1;
-                if(bit) temp.push_back(nums[idx]);
-                idx++;
-                k >>=1;
-            }
-            ans.push_back(temp);
-            
-        }
+        generate(nums, idx + 1, temp);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>ans;
-        // recurse(ans, nums, 0);
-        iterative(ans, nums);
+        vector<int>temp;
+        generate(nums, 0, temp);
         return ans;
     }
 };
