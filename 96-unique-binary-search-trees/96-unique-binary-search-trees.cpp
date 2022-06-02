85d4a1299d7c7224af3d59cc601b397a8ea19ecc
Rajath V <f20190122@goa.bits-pilani.ac.in>
2022-06-02 13:00:46 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (12.56%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -4,11 +4,13 @@ public:
         vector<int>dp(n + 1);
         dp[0] = 1;
         dp[1] = 1;
-        for(int i = 2; i <=n; ++i){
-            for(int j = 0; j <i; ++j){
-                dp[i] += dp[j]*dp[i-j-1];
+        
+        for(int i = 2; i <= n; ++i){
+            for(int j = 1; j <= i; ++j){
+                dp[i] += dp[j-1]*dp[i-j];
             }
         }
         return dp[n];
+        
     }
 };
