a723f2c9bf708e5befc00091657650b18f2747c9
Rajath V <f20190122@goa.bits-pilani.ac.in>
2022-06-02 10:21:29 +0530

    Time: 192 ms (41.67%), Space: 83.9 MB (27.64%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -3,27 +3,30 @@ public:
     int findMaxLength(vector<int>& nums) {
         for(auto &x : nums) if(x == 0) x = -1;
         unordered_map<int,int>mp;
-
-        
         mp[0] = -1;
+        int ans = 0;
         int sum = 0;
-        int res = INT_MIN;
         
-        for(int i=0;i<nums.size();i++){
-            //cumulative sum
-            sum+=nums[i];
-            
-            //check if value already exists in the map
-            if(mp.find(sum) != mp.end()){
-                //finding the length of possible subarray and comparing with the max result
-                res=max(res,i-mp[sum]);
-            }
-            else{
-                //adding value to our map
-                mp[sum]=i;
+        for(int i = 0; i < nums.size(); ++i){
+            sum += nums[i];
+            if(mp.count(sum)){
+                ans = max(ans, abs(i - mp[sum]));
+            }else{
+                mp[sum] = i;
             }
         }
-        return res == INT_MIN ? 0 : res;
+        return ans;
+        
         
     }
 };
+
+/*
+[0,1,0,0,1,0,1,0,1]
+
+[-1,1,-1,-1,1,-1,1,-1,1]
+
+i = 0, j = 0;
+
+
+*/
