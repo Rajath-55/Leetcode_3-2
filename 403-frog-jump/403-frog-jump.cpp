6a227185fbba6c48a974b529264e9cc4e837cc48
Rajath V <f20190122@goa.bits-pilani.ac.in>
2022-06-02 12:57:09 +0530

    Time: 655 ms (12.84%), Space: 232.1 MB (14.74%) - LeetHub

--- 403-frog-jump/403-frog-jump.cpp
+++ 403-frog-jump/403-frog-jump.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     bool canCross(vector<int>& stones) {
-        int n = stones.size();
-        vector<unordered_set<int>>dp(n);
+        vector<vector<int>>dp(stones.size(), vector<int>(stones.size() + 1));
         
-        dp[0].insert(1);
+        dp[0][1] = 1;
         
         for(int i = 1; i < stones.size(); ++i){
             for(int j = 0; j < i; ++j){
                 int dist = stones[i] - stones[j];
-                if(!dp[j].count(dist)) continue;
-                dp[i].insert(dist);
-                if(dist + 1 < n) dp[i].insert(dist + 1);
-                if(dist - 1 >= 0) dp[i].insert(dist - 1);
+                if(dist >= stones.size() or !dp[j][dist] or dist < 0) continue;
+                
+                dp[i][dist] = 1;
+                if(dist > 0) dp[i][dist - 1] = 1;
+                if(dist < stones.size() - 1) dp[i][dist + 1] = true;
+                if(i == stones.size() - 1) return true;
             }
         }
-        return !dp[n-1].empty();
         
+        return false;
         
     }
 };
