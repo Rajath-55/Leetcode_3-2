fd621cc827c95b5f3715346d41c9bd8efbb4c9aa
Rajath V <f20190122@goa.bits-pilani.ac.in>
2022-06-10 18:49:05 +0530

    Time: 4 ms (80.01%), Space: 7.2 MB (43.15%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
-    string s,p;
-    map<pair<int,int>, bool>cache;
-    
-    bool recurse(int sidx, int pidx){
-        if(sidx >= s.length() and pidx >= p.length()) return true;
-        if(pidx >= p.length()) return false;
+    bool isMatch(string s, string p) {
+        int n = s.length();
+        int m = p.length();
         
-        if(cache.count({sidx, pidx})) return cache[{sidx, pidx}];
+        vector<vector<int>>dp(n + 1, vector<int>(m+1));
+        dp[n][m] = 1;
         
-        bool isEqual = sidx < s.length() and (s[sidx] == p[pidx] or p[pidx] == '.');
-        
-        if(pidx + 1 < p.length() and p[pidx + 1] == '*'){
-            // go left (dont use the star), or right(use the star)
-            if(recurse(sidx, pidx + 2) or (isEqual and recurse(sidx + 1, pidx))){
-                return cache[{sidx,pidx}] = true;
+        for(int i = n; i >=0; --i){
+            for(int j = m - 1; j >= 0; --j){
+                bool match = (i < n and (p[j] == '.' or s[i] == p[j]));
+                
+                if(j < m - 1 and p[j + 1] == '*'){
+                    dp[i][j] = dp[i][j+2] or (match and dp[i+1][j]);
+                }else{
+                    dp[i][j] = match and dp[i+1][j+1];
+                }
+                
             }
         }
-        if(isEqual){
-            return cache[{sidx,pidx}] = recurse(sidx+1, pidx + 1);
-        }
-        return cache[{sidx,pidx}] = false;
-               
-        
-    }
-    bool isMatch(string s, string p) {
-        this->s = s;
-        this->p = p;
-        
-        // every time we encounter a * we either use it (and check for previous character) or we dont use it.
-        return recurse(0, 0);
-    
-        
+        return dp[0][0];
     }
 };
