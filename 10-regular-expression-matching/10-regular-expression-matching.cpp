7d00bc245fefa252083411afefb0adecbef77445
Rajath V <f20190122@goa.bits-pilani.ac.in>
2022-06-10 16:19:47 +0530

    Time: 35 ms (29.25%), Space: 16.8 MB (13.09%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     string s,p;
-    vector<vector<int>>dp;
+    map<pair<int,int>, bool>cache;
     
     bool recurse(int sidx, int pidx){
         if(sidx >= s.length() and pidx >= p.length()) return true;
         if(pidx >= p.length()) return false;
+        
+        if(cache.count({sidx, pidx})) return cache[{sidx, pidx}];
         
         bool isEqual = sidx < s.length() and (s[sidx] == p[pidx] or p[pidx] == '.');
         
         if(pidx + 1 < p.length() and p[pidx + 1] == '*'){
             // go left (dont use the star), or right(use the star)
             if(recurse(sidx, pidx + 2) or (isEqual and recurse(sidx + 1, pidx))){
-                return true;
+                return cache[{sidx,pidx}] = true;
             }
         }
         if(isEqual){
-            return recurse(sidx+1, pidx + 1);
+            return cache[{sidx,pidx}] = recurse(sidx+1, pidx + 1);
         }
-        return false;
+        return cache[{sidx,pidx}] = false;
                
         
     }
     bool isMatch(string s, string p) {
         this->s = s;
         this->p = p;
-        int n = s.length(), m = p.length();
-        dp.resize(n, vector<int>(m,-1));
         
         // every time we encounter a * we either use it (and check for previous character) or we dont use it.
         return recurse(0, 0);
