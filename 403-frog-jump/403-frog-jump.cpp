ee17abba0739f84bb1693761d7354bd0c79727bd
Rajath V <f20190122@goa.bits-pilani.ac.in>
2022-06-16 12:03:52 +0530

    Time: 188 ms (68.85%), Space: 249.4 MB (5.99%) - LeetHub

--- 403-frog-jump/403-frog-jump.cpp
+++ 403-frog-jump/403-frog-jump.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
+    unordered_map<long long, int> getID;
+    vector<vector<int>> dp;
+    vector<long long> a;
+    int n;
+    
+    bool solve ( int i, int k ) {
+        if ( i == n-1 ) return 1;
+        if ( dp[i][k] != -1 ) return dp[i][k];
+        
+        bool ret = 0;
+        long long g;
+        
+        g = a[i] + k - 1;
+        int id = getID[g];
+        if ( id != 0 && k-1 > 0 ) ret |= solve ( id, k-1 );
+        
+        g = a[i] + k;
+        id = getID[g];
+        if ( id != 0 && k > 0 ) ret |= solve ( id, k );
+        
+        g = a[i] + k + 1;
+        id = getID[g];
+        if ( id != 0 ) ret |= solve ( id, k+1 );
+        
+        return dp[i][k] = ret;
+    }
+    
     bool canCross(vector<int>& stones) {
-        vector<vector<int>>dp(stones.size(), vector<int>(stones.size() + 1));
+        n = stones.size();
         
-        dp[0][1] = 1;
-        
-        for(int i = 1; i < stones.size(); ++i){
-            for(int j = 0; j < i; ++j){
-                int dist = stones[i] - stones[j];
-                if(dist >= stones.size() or !dp[j][dist] or dist < 0) continue;
-                
-                dp[i][dist] = 1;
-                if(dist > 0) dp[i][dist - 1] = 1;
-                if(dist < stones.size() - 1) dp[i][dist + 1] = true;
-                if(i == stones.size() - 1) return true;
-            }
+        for ( int i = 0; i < n; i++ ) {
+            getID[stones[i]] = i;
+            a.push_back ( stones[i] );
         }
         
-        return false;
-        
+        dp.resize( n, vector<int>(n, -1) );
+        return solve ( 0, 0 );
     }
 };
