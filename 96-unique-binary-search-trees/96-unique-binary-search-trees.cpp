5596f6681d2241af1b40318ca26c6d926f5346c0
Rajath V <f20190122@goa.bits-pilani.ac.in>
2022-06-24 19:46:06 +0530

    Time: 3 ms (38.15%), Space: 6.1 MB (11.80%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-    int numTrees(int n) {
-        vector<int>dp(n + 1);
-        dp[0] = 1;
-        dp[1] = 1;
+    
+    vector<vector<int>>dp;
+    
+    int recurse(int start, int end){
+        if(start > end) return 1;
+        if(start == end) return 1;
+        if(dp[start][end] != -1) return dp[start][end];
         
-        for(int i = 2; i <= n; ++i){
-            for(int j = 1; j <= i; ++j){
-                dp[i] += dp[j-1]*dp[i-j];
-            }
+        int ans = 0;
+        for(int i = start; i <= end; ++i){
+            ans += recurse(start, i - 1)*recurse(i + 1, end);
         }
-        return dp[n];
+        return dp[start][end] = ans;
         
+    }
+    
+    int numTrees(int n) {
+        dp.resize(22, vector<int>(22, -1));
+        return recurse(1, n);
     }
 };
