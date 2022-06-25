0514dcdb57f6ee46a02e93efa40adede56a5a1be
Rajath V <f20190122@goa.bits-pilani.ac.in>
2022-06-26 00:10:00 +0530

    Time: 275 ms (27.85%), Space: 79.9 MB (33.60%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
-    vector<vector<bool>>dp;
+    vector<vector<int>>dp;
     vector<vector<string>>ans;
-    void dfs(int start, vector<string>temp, string &s){
-        if(start == s.length()){
+    void backtrack(vector<string>&temp, int idx, string s){
+        if(idx == s.length()){
             ans.push_back(temp);
             return;
         }
-        for(int i = start; i < s.length(); ++i){
-            if(dp[start][i]){
-                temp.push_back(s.substr(start, i - start + 1));
-                dfs(i + 1, temp, s);
+        
+        for(int i = idx; i < s.length(); ++i){
+            if(dp[idx][i]){
+                temp.push_back(s.substr(idx, i - idx + 1));
+                backtrack(temp, i + 1, s);
                 temp.pop_back();
             }
         }
-        return;
     }
     vector<vector<string>> partition(string s) {
         int n = s.length();
-        dp.resize(n, vector<bool>(n));
-        
+        dp.resize(n, vector<int>(n));
         for(int i = 0; i < n; ++i){
             int start = i, end = i;
-            while(start >=0 and end < n and s[start] == s[end]){
-                dp[start--][end++] = 1;
+            while(start >= 0 and end < n and s[start] == s[end]){
+                dp[start][end] = 1;
+                start--;
+                end++;
             }
             start = i;
             end = i + 1;
-            while(start >=0 and end < n and s[start] == s[end]){
-                dp[start--][end++] = 1;
+            while(start >= 0 and end < n and s[start] == s[end]){
+                dp[start][end] = 1;
+                start--;
+                end++;
             }
         }
         vector<string>temp;
-        dfs(0, temp,s);
+        backtrack(temp, 0, s);
         return ans;
-        
     }
 };
