710766d993cbf381a2be45c270ed646477908a21
Rajath V <f20190122@goa.bits-pilani.ac.in>
2022-07-14 22:09:38 +0530

    Time: 19 ms (66.72%), Space: 12.2 MB (39.61%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
-    int c=0;
-    void func(vector<int>&n,int t,int i)
-    {
-      if(i>=n.size())
-      {if(t==0)
-          c++;return;}
-      func(n,t+n[i],i+1);
-      func(n,t-n[i],i+1);  
-    }
-    int findTargetSumWays(vector<int>&n, int target) {
-        func(n,target,0);
-        return c;
+    vector<vector<int>>dp;
+    
+    int dfs(vector<int>&nums, int target, int idx){
+        if(idx == nums.size()) return target == 0;
+        if(target < 0) return 0;
+        
+        if(dp[idx][target] != -1) return dp[idx][target];
+        
+        int take = dfs(nums, target - nums[idx], idx + 1);
+        int dontTake = dfs(nums, target, idx + 1);
+        return dp[idx][target] = take + dontTake;
     }
     
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum = 0;
+        for(auto x : nums) sum+=x;
+        if((sum + target)%2 == 1 or sum + target < 0 or target > sum) return 0;
+        
+        target = (sum + target)/2;
+        dp.resize(nums.size(), vector<int>(target + 1, -1));
+        return dfs(nums, target, 0);
+        
+    }
 };
