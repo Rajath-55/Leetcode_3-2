1cb21cfeb6532c7b4833df80f019bc19e42937a5
Rajath V <f20190122@goa.bits-pilani.ac.in>
2022-07-14 21:09:03 +0530

    Time: 50 ms (94.13%), Space: 140 MB (8.74%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-
+    vector<vector<int>>dp;
     
-    
+    int dfs(vector<int>&A, int idx, int target){
+        if(idx == A.size()) return target == 0;
+        if(target < 0) return 0;
+        if(target == 0) return 1;
+        if(dp[idx][target] != -1) return dp[idx][target];
+        
+        return dp[idx][target] = dfs(A, idx + 1, target - A[idx]) || dfs(A, idx + 1, target);
+    }
     bool canPartition(vector<int>& nums) {
-        int n = nums.size();
+        // sum of subsets are equal. 
+        //partition implies that s1 U s2 = S 
+        // let us consider the sum of subset one = two = x.
+        // totalsum = 2x = A
+        // A/2 possible as a subset.
+        // this boils down to subset sum with total = A
         int sum = accumulate(nums.begin(), nums.end(), 0);
         if(sum&1) return false;
-        int half = sum/2;
-        vector<vector<int>>dp(n, vector<int>(half + 1));
-        
-        for(int i = 0; i < n; ++i) dp[i][0] = 1;
-        
-        for(int i = 1; i < n; ++i){
-            for(int j = 0; j <= half; ++j){
-                if(j - nums[i-1] >= 0){
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
-                }else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        return dp[n-1][half] == 1;
-        //choose elements to sum to half now that is the question.
-        //dp[i][j] can we taje first i elements to sum j. 
-        
-        
+        sum/=2;
+        dp.resize(nums.size(), vector<int>(sum*2, -1));
+        return dfs(nums, 0, sum);
     }
 };
