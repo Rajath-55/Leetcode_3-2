fe4daf655a961b95418c46b17ca74becb2e76fd2
Rajath V <f20190122@goa.bits-pilani.ac.in>
2022-07-26 09:44:04 +0530

    Time: 22 ms (64.82%), Space: 14.2 MB (57.81%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -11,11 +11,13 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(not root) return NULL;
+        
         if(root == p or root == q) return root;
         
         auto left = lowestCommonAncestor(root->left, p, q);
         auto right = lowestCommonAncestor(root->right, p, q);
         if(left and right) return root;
+        
         return left ? left : right;
     }
 };
