147227d0f81f777e945dc58eaa99fc1f93fe4660
Rajath V <f20190122@goa.bits-pilani.ac.in>
2022-08-06 01:06:34 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (44.84%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -12,30 +12,72 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        int zigzag = 0;
-        if(not root) return {};
-        queue<TreeNode *>q;
+        // handle the edge case for the input when the root is null:
+        vector<vector<int>>zigzagList;
+        if(root == nullptr){
+            // we return an empty array.
+            return zigzagList;
+        }
+        
+        // level 1 -> (3) (order left to right)
+        // level 2 -> (9,20) (order right to left).
+        // level 3 -> (15, 7) (order left to right)   note: null children as they are not added into the level order traversal.
+        queue<TreeNode*>q;
         q.push(root);
-        vector<vector<int>>ans;
+        bool isLeft = true;
         
         while(!q.empty()){
             int size = q.size();
-            vector<int>temp;
+            vector<int>levelOrderValues;
             
             for(int i = 0; i < size; ++i){
-                auto front = q.front();
+                TreeNode* front = q.front();
                 q.pop();
-                temp.push_back(front->val);
-                if(front->left) q.push(front->left);
-                if(front->right) q.push(front->right);
+                levelOrderValues.push_back(front->val);
+                
+                if(front->left){
+                    q.push(front->left);
+                }
+                if(front->right){
+                    q.push(front->right);
+                }
             }
-            if(zigzag){
-                reverse(temp.begin(), temp.end());
+            if(!isLeft){
+                reverse(levelOrderValues.begin(), levelOrderValues.end());
             }
-            ans.push_back(temp);
-            zigzag = !zigzag;
+            isLeft = !isLeft;
+            zigzagList.push_back(levelOrderValues);
         }
-        return ans;
+        return zigzagList;
         
     }
 };
+/*
+clarifying questions:
+A node with value.
+root = could be null.
+[3] 
+root = null -> The expected output is {{}};
+
+perfect? complete? or any random binary tree.
+
+3
+
+9 20
+  15 7.   -> [[3], [20, 9], [15,7]];
+  
+q -> (3)
+in every iteration of the traversal, we pop all the elements in the current level, and add their children.
+
+first iteration -> q = 3. Pop 3, add its children in left to right fashion. -> 9 20
+second iteration -> q = 9 20 pop 9, add its non null children 20 pop 20 add its non null children, 15 7
+third iteration -> q = 15 7 pop 15 add its non null children pop 7 add its non null children. -> empty.
+
+We move out of the level order traversal loop because there are no more children to be added that are not null.
+
+therefore now, we have to now store the elements in zigzag fashion, so we maintain a boolean isLeft.
+isLeft is true -> we store elements as we add them into the temporary array.
+else we reverse the array and store it.
+
+
+*/
